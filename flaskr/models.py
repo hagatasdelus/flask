6a9a156f62d5aefdd0f96f0f9c33b8ece552e742
hagatasdelus <gsiyakav.c08nk@gmail.com>
2023-07-30 11:12:33 +0900

    transaction add

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,3 +1,20 @@
+from flaskr import db, login_manager
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_login import UserMixin, current_user
+from sqlalchemy.orm import aliased #参照先の複数テーブルを紐づけたい時に使う
+from sqlalchemy import and_, or_, desc
+from datetime import datetime, timedelta
+from contextlib import contextmanager
+
+@contextmanager
+def transaction():
+    try:
+        yield
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        raise
+
 class BookInfo:
     def __init__(self, number, title, genre, price, arrival_day, picture_path):
         self.number = number
